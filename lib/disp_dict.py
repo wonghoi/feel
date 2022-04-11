4dc2088309bace5d4a354cd58de554204c30aed1
wonghoi <wonghoi+github@humgar.com>
2022-04-10 20:23:24 -0700

    Allow display of non-string types in disp_dict

--- lib/disp_dict.py
+++ lib/disp_dict.py
@@ -12,14 +12,19 @@ def print_dict_item(item, N_key=20, N_val=70, separator_str=' : ', shortener=Non
     
     sep = separator_str
     
-    rhs = val
+    try:
+        rhs = str(val)
+    except:
+        raise TypeError("Type cannot be cased into string for display")
+        
     N_rhs_indent = N_key+len(sep)
     rhs_indent   = ' '*N_rhs_indent
-    if( len(val)>N_val ):
+    if( len(rhs)>N_val ):
         if shortener is None:
             rhs = textwrap.fill(rhs, width=N_val, initial_indent=rhs_indent, subsequent_indent=rhs_indent).lstrip()
         else:
             rhs = textwrap.shorten(rhs, width=N_val, placeholder=shortener)
+                
             
     lines = lhs + sep + rhs    
     return lines
